268c41e446036b2df01669c2757efa8f15267b94
simonjvardy <sjvardy@gmail.com>
2021-04-08 17:06:39 +0000

    Update allauth templates and profiles functionality

--- profiles/views.py
+++ profiles/views.py
@@ -7,10 +7,13 @@ from .models import UserProfile
 from checkout.models import Order
 
 
-
 def profile(request):
     """ Display the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
