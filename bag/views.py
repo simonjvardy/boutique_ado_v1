8bc7466e09ef4ae1af32b8dcf07309fde9752e91
simonjvardy <sjvardy@gmail.com>
2021-04-10 12:13:41 +0000

    Updated the entire repo from the CI walkthrough

--- bag/views.py
+++ bag/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import (
-    render, redirect, reverse, HttpResponse, get_object_or_404)
+    render, redirect, reverse, HttpResponse, get_object_or_404
+)
 from django.contrib import messages
-from products.models import Product
 
-# Create your views here.
+from products.models import Product
 
 
 def view_bag(request):
-    """A view that renders the bag contents page"""
+    """ A view that renders the bag contents page """
+
     return render(request, 'bag/bag.html')
 
 
@@ -26,39 +27,36 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
-                messages.success(
-                    request,
-                    f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
+                messages.success(request,
+                                 (f'Updated size {size.upper()} '
+                                  f'{product.name} quantity to '
+                                  f'{bag[item_id]["items_by_size"][size]}'))
             else:
                 bag[item_id]['items_by_size'][size] = quantity
-                messages.success(
-                    request,
-                    f'Added size {size.upper()} {product.name} to your bag')
+                messages.success(request,
+                                 (f'Added size {size.upper()} '
+                                  f'{product.name} to your bag'))
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
-            messages.success(
-                request,
-                f'Added size {size.upper()} {product.name} to your bag')
+            messages.success(request,
+                             (f'Added size {size.upper()} '
+                              f'{product.name} to your bag'))
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
-            messages.success(
-                request,
-                f'Updated {product.name} quantity to {bag[item_id]}')
+            messages.success(request,
+                             (f'Updated {product.name} '
+                              f'quantity to {bag[item_id]}'))
         else:
             bag[item_id] = quantity
-            messages.success(
-                request,
-                f'Added {product.name} to your bag')
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
 
 
 def adjust_bag(request, item_id):
-    """
-    Adjust the quantity of the specified product to the specified amount
-    """
+    """Adjust the quantity of the specified product to the specified amount"""
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
@@ -70,36 +68,35 @@ def adjust_bag(request, item_id):
     if size:
         if quantity > 0:
             bag[item_id]['items_by_size'][size] = quantity
-            messages.success(
-                request,
-                f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
+            messages.success(request,
+                             (f'Updated size {size.upper()} '
+                              f'{product.name} quantity to '
+                              f'{bag[item_id]["items_by_size"][size]}'))
         else:
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-            messages.success(
-                request,
-                f'Removed size {size.upper()} {product.name} from your bag')
+            messages.success(request,
+                             (f'Removed size {size.upper()} '
+                              f'{product.name} from your bag'))
     else:
         if quantity > 0:
             bag[item_id] = quantity
-            messages.success(
-                request,
-                f'Updated {product.name} quantity to {bag[item_id]}')
+            messages.success(request,
+                             (f'Updated {product.name} '
+                              f'quantity to {bag[item_id]}'))
         else:
             bag.pop(item_id)
-            messages.success(
-                request,
-                f'Removed {product.name} from your bag')
+            messages.success(request,
+                             (f'Removed {product.name} '
+                              f'from your bag'))
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
 
 def remove_from_bag(request, item_id):
-    """
-    Remove item from the shopping bag
-    """
+    """Remove the item from the shopping bag"""
 
     try:
         product = get_object_or_404(Product, pk=item_id)
@@ -112,14 +109,12 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-            messages.success(
-                request,
-                f'Removed size {size.upper()} {product.name} from your bag')
+            messages.success(request,
+                             (f'Removed size {size.upper()} '
+                              f'{product.name} from your bag'))
         else:
             bag.pop(item_id)
-            messages.success(
-                request,
-                f'Removed {product.name} from your bag')
+            messages.success(request, f'Removed {product.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
--- home/views.py
+++ home/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 
 
-# Create your views here.
-
-
 def index(request):
-    """A view to render the index"""
+    """ A view to return the index page """
+
     return render(request, 'home/index.html')
